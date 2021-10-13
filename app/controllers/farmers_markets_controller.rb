cdb5b80717ed2a24ec0202b52318dd55a241f439
matthewjholmes <ilookup@gmail.com>
2021-10-13 16:29:33 -0600

    Implement Restaurants index on user story 1"

---
+++ app/controllers/farmers_markets_controller.rb
@@ -0,0 +1,11 @@
+class FarmersMarketsController < ApplicationController
+
+  def index
+    @farmers_markets = FarmersMarket.all
+  end
+
+  def show
+    @farmers_market = FarmersMarket.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/restaurants', to: 'restaurants#index'
+
+  get '/farmers_markets', to: 'farmers_markets#index'
 end
