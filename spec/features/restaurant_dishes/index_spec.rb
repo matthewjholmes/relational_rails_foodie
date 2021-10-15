41c58b76476c39c0138423e77eff71bd8a4eeb33
matthewjholmes <ilookup@gmail.com>
2021-10-15 16:55:28 -0600

    Implement new/create method for restaurant dishes and farmers market produce, user story 13 complete

--- spec/features/restaurant_dishes/index_spec.rb
+++ spec/features/restaurant_dishes/index_spec.rb
@@ -27,5 +27,21 @@ RSpec.describe 'Restaurant Dishes Index' do
       expect(page).to have_content(@dish3.calories)
       expect(page).to have_content(@dish3.restaurant_id)
     end
+
+    it 'i see a link to add a new dish' do
+      click_link 'Create New Dish'
+      expect(current_path).to eq("/restaurants/#{@restaurant1.id}/dishes/new")
+    end
+
+    it 'i fill out the form and a new dish is created, and redirect to restuarant dishes index' do
+      click_link 'Create New Dish'
+      fill_in 'Name', with: 'Burrito'
+      fill_in 'Seasonal (true/false)', with: 'false'
+      fill_in 'Calories', with: '1200'
+      click_button 'Create New Dish'
+
+      expect(current_path).to eq("/restaurants/#{@restaurant1.id}/dishes")
+      expect(@restaurant1.dishes.count).to eq(4)
+    end
   end
 end
