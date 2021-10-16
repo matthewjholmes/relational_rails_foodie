8822dfd5e1fdc63d22baa9fca8521147b8f07e7b
Devin Pile <devinpile@gmail.com>
2021-10-16 09:59:50 -0600

    Add changes to implement story14

--- app/controllers/produces_controller.rb
+++ app/controllers/produces_controller.rb
@@ -7,4 +7,23 @@ class ProducesController < ApplicationController
   def show
     @produce = Produce.find(params[:id])
   end
+
+  def edit
+    @produce = Produce.find(params[:id])
+  end
+
+  def update
+    produce = Produce.find(params[:id])
+    if produce.update(produce_params)
+      redirect_to "/produces/#{produce.id}"
+    else
+      redirect_to "/produces/#{produces.id}/edit"
+    end
+  end
+
+  private
+
+  def produce_params
+    params.permit(:name, :seasonal, :quantity)
+  end
 end
